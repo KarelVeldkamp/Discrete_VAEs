made mixture difficulties more similar across classes, fixed bug in mse(theta) computation

MIXIRT.py
@@ -277,7 +277,10 @@ def sim_mixirt_pars(N, nitems, nclass, mirt_dim, Q, class_prob=.5, cov=0):
     covMat = np.full((mirt_dim, mirt_dim), cov)  # covariance matrix of dimensions, zero for now
     np.fill_diagonal(covMat, 1)
     true_theta = np.random.multivariate_normal([0] * mirt_dim, covMat, N)
-    true_difficulty = np.random.uniform(-2, 2, (nitems, 2))
+    true_difficulty = np.repeat(np.random.uniform(-2, 2, (nitems, 1)), 2, axis=1)
+    true_difficulty[:,1] += np.random.normal(0,.1, true_difficulty.shape[0])
+
+
     # true_slopes = np.random.uniform(.5, 2, (cfg['nitems'], cfg['mirt_dim'],2))
     true_slopes = np.repeat(np.random.uniform(.5, 2, (nitems, mirt_dim, 1)), 2, -1)
     true_slopes *= np.expand_dims(Q, -1)

main.py
@@ -51,7 +51,7 @@ if cfg['SimConfigs']['sim_data']:
         true_theta = None
     elif cfg['GeneralConfigs']['model'] == 'MIXIRT':
         if cfg['ModelSpecificConfigs']['mirt_dim'] > 1:
-            Q = pd.read_csv(f'./QMatrices/QMatrix{cfg["ModelSpecificConfigs"]["mirt_dim"]}D.csv', header=None).values.astype(float)
+            Q = pd.read_csv(f'./Qmatrices/QMatrix{cfg["ModelSpecificConfigs"]["mirt_dim"]}D.csv', header=None).values.astype(float)
         else:
             Q = np.ones((cfg['SimConfigs']['n_items'], cfg['ModelSpecificConfigs']['mirt_dim']))
 
@@ -208,14 +208,14 @@ elif cfg['GeneralConfigs']['model'] == 'GDINA':
 if cfg['GeneralConfigs']['model'] == 'MIXIRT':
     # The latent dimension in the mixture is only identified up to the sign so we might have to flip the sign:
     for dim in range(theta.shape[1]):
-        print(pearsonr(true_theta[:, dim], theta[:, dim]).statistic)
         if pearsonr(true_theta[:, dim], theta[:, dim]).statistic < 0:
             best_theta[:, dim] *= -1
             best_itempars[:,dim+1, :] *= -1
 
-    mse_theta = MSE(best_theta, true_theta)
-    bias_theta = np.mean(best_theta - true_theta)
-    var_theta = np.var(best_theta)
+
+    mse_theta = MSE(best_theta.detach().numpy(), true_theta)
+    bias_theta = np.mean(best_theta.detach().numpy() - true_theta)
+    var_theta = np.var(best_theta.detach().numpy())
 else:
     mse_theta = bias_theta = var_theta = None
 