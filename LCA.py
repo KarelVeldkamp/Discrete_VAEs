fixed bug for VQvae loglikelihood computation

LCA.py
@@ -280,7 +280,7 @@ class LCA(pl.LightningModule):
             pi[torch.arange(data.shape[0]), pred_class_ix] = 1
 
             embs = self.sampler.embeddings.weight
-            est_probs = self.decoder(embs)
+            est_probs = self.decoder(embs).T 
 
         log_likelihood = torch.sum(data * torch.log(pi@est_probs.T + 1e-10) +
                                 (1 - data) * torch.log(1 - pi@est_probs.T + 1e-10))

main.py
@@ -230,11 +230,10 @@ else:
 
 
 # compute MSE of conditional probabilities
-mse_itempars = MSE(best_itempars[true_itempars!=0].detach().numpy(), true_itempars[true_itempars!=0])
+mse_itempars = MSE(best_itempars.detach().numpy()[true_itempars!=0], true_itempars[true_itempars!=0])
 
-mse_itempars = MSE(best_itempars[true_itempars!=0].detach().numpy(), true_itempars[true_itempars!=0])
-bias_itempars = np.mean(best_itempars[true_itempars!=0].detach().numpy() - true_itempars[true_itempars!=0])
-var_itempars = np.var(best_itempars[true_itempars!=0].detach().numpy())
+bias_itempars = np.mean(best_itempars.detach().numpy()[true_itempars!=0] - true_itempars[true_itempars!=0])
+var_itempars = np.var(best_itempars.detach().numpy()[true_itempars!=0])
 
 
 