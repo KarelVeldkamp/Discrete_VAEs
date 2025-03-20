fix temperature numerical issue in mixirt

GDINA.py
@@ -119,7 +119,7 @@ class GDINA(pl.LightningModule):
                                                         probs=torch.ones_like(pi)).log_prob(z).sum(-1)
             # posterior probability of the samples p(z|x)
             log_q_theta_x = dist.RelaxedOneHotCategorical(torch.Tensor([self.sampler.temperature]),
-                                                          probs=pi).log_prob(z).sum(-1)
+                                                          probs=pi.detach()).log_prob(z).sum(-1)
 
             # kl divergence
             kl = (log_q_theta_x - log_p_theta).unsqueeze(-1)  # kl divergence
@@ -130,6 +130,8 @@ class GDINA(pl.LightningModule):
             # do importance weighting
             with torch.no_grad():
                 weight = (elbo - elbo.logsumexp(dim=0)).exp()
+                if z.requires_grad:
+                    z.register_hook(lambda grad: (weight.unsqueeze(-1) * grad).float())
             loss = (-weight * elbo).sum(0).mean()
 
         return loss, weight
@@ -163,7 +165,8 @@ class GDINA(pl.LightningModule):
 
         if self.n_samples == 1:
             logits = self.encoder(data)
-            logits = logits.reshape((logits.shape[0], logits.shape[1] // 2, 2)).exp()
+            logits = torch.nn.functional.softplus(logits.reshape((logits.shape[0], logits.shape[1] // 2, 2)))
+            #logits = logits.reshape((logits.shape[0], logits.shape[1] // 2, 2)).exp()
             mu = self.sampler.softmax(logits)[:, :, 0]
             return mu.unsqueeze(0)
         else:
@@ -200,6 +203,7 @@ class GDINA(pl.LightningModule):
         data = torch.Tensor(data)
         pi = self.fscores(torch.Tensor(data)).mean(0)
 
+
         pi_att = expand_interactions(pi).squeeze()
         pi_att = torch.cat((torch.ones(pi_att.shape[0]).unsqueeze(-1), pi_att), -1)
 
@@ -209,8 +213,6 @@ class GDINA(pl.LightningModule):
 
         log_likelihood = torch.sum(data * torch.log(pi_att @ delta.T + 1e-6) +
                                    (1 - data) * torch.log(1 - (pi_att @ delta.T - 1e-6)))
-
-        print(log_likelihood)
 
 
         delta = delta.unsqueeze(1)

MIXIRT.py
@@ -163,30 +163,33 @@ class VAE(pl.LightningModule):
         log_p_x_theta = ((input * reco).clamp(1e-7).log() + ((1 - input) * (1 - reco)).clamp(1e-7).log()) # compute log ll
         logll = (log_p_x_theta * mask).sum(dim=-1, keepdim=True) # set elements based on missing data_pars to zero
         #
-        cl = cl / cl.sum(dim=-1, keepdim=True)
+        #cl = cl / cl.sum(dim=-1, keepdim=True)
+
+        cl = torch.clamp(cl, min=1e-8)  # Ensure strictly positive
+        cl = cl / cl.sum(-1, keepdim=True)  # Re-normalize for numerical safety
+
+        #print(torch.max())
+
         # calculate normal KL divergence
         log_q_theta_x = torch.distributions.Normal(mu.detach(), sigma.exp().detach()+ 1e-7).log_prob(z).sum(dim = -1, keepdim = True) # log q(Theta|X)
         log_p_theta = torch.distributions.Normal(torch.zeros_like(z).to(input), scale=torch.ones(mu.shape[2]).to(input)).log_prob(z).sum(dim = -1, keepdim = True) # log p(Theta)
         kl_normal =  log_q_theta_x - log_p_theta # kl divergence
 
         # calculate concrete KL divergence
-        pi = torch.clamp(pi, min=1e-7, max=1 - 1e-7)
-        cl = torch.clamp(cl, min=1e-7, max=1 - 1e-7)
+        #pi = torch.clamp(pi, min=1e-7, max=1 - 1e-7)
+        #cl = torch.clamp(cl, min=1e-7, max=1 - 1e-7)
 
-
-
+        unif_probs = torch.full_like(pi, 1.0 / pi.shape[-1])
         log_p_cl = torch.distributions.RelaxedOneHotCategorical(torch.Tensor([self.GumbelSoftmax.temperature]).to(pi),
-                                                    probs=torch.ones_like(pi)).log_prob(cl).unsqueeze(-1)
+                                                    probs=unif_probs).log_prob(cl).unsqueeze(-1)
 
         log_q_cl_x = torch.distributions.RelaxedOneHotCategorical(torch.Tensor([self.GumbelSoftmax.temperature]).to(pi),
                                                       probs=pi).log_prob(cl).unsqueeze(-1)
 
         kl_concrete = (log_q_cl_x - log_p_cl)
 
-
-
         # combine into ELBO
-        elbo = logll - kl_normal - kl_concrete
+        elbo = logll - kl_normal #- kl_concrete
 
 
         # # perform importance weighting